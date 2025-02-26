45f3bd5ab2fbc55040d2df0d87ea0b577d3f4bb1
Yasmin Macedo <yasminmacedo_11@outlook.com>
2025-02-26 01:44:21 -0300

    feat: criando design da header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,23 @@
+import logo from "../../assets/Logo (Stroke).png"
+import profile from "../../assets/Profile.png"
+import shop from "../../assets/Shop.png"
+import styles from "./styles.module.css"
+
 
 export default function Header() {
     return (
-        <header>
-            <h1>Header</h1>
-            <hr/>
+        <header className={styles.container}>
+            <nav>
+                <img className={styles.logo} src={logo} alt="Logo da Livraria"/>
+                <div>
+                    <button className={styles.buttons}>
+                        <img src={profile} alt="Logo da Livraria"/>
+                    </button>
+                    <button className={styles.buttons}>
+                        <img src={shop} alt="Logo da Livraria"/>
+                    </button>
+                </div>
+            </nav>
         </header>
     )
 }

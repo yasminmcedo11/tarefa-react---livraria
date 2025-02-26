3e3a03d022927367b24c1c0833df430ebcf313fa
Yasmin Macedo <yasminmacedo_11@outlook.com>
2025-02-26 01:54:03 -0300

    implementando funcionalidades dos botões de redirecionamento do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom"
 import logo from "../../assets/Logo (Stroke).png"
 import profile from "../../assets/Profile.png"
 import shop from "../../assets/Shop.png"
@@ -5,12 +6,18 @@ import styles from "./styles.module.css"
 
 
 export default function Header() {
+    const navigate = useNavigate()
+
     return (
         <header className={styles.container}>
             <nav>
-                <img className={styles.logo} src={logo} alt="Logo da Livraria"/>
+                <img className={styles.logo} 
+                     src={logo} 
+                     alt="Logo da Livraria"
+                     onClick={() => navigate("/")}/>
                 <div>
-                    <button className={styles.buttons}>
+                    <button className={styles.buttons}
+                            onClick={() => navigate("/login")}>
                         <img src={profile} alt="Logo da Livraria"/>
                     </button>
                     <button className={styles.buttons}>

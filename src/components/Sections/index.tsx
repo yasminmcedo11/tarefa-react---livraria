dd4c723115709ce1f412f4c97d06c50687192483
Yasmin Macedo <yasminmacedo_11@outlook.com>
2025-03-05 20:49:22 -0300

    feat(home): implementando requisicoes para renderizacao dos livros de cada genero

--- src/components/Sections/index.tsx
+++ src/components/Sections/index.tsx
@@ -1,28 +1,66 @@
 import ShowMoreButton from "../Buttons/ShowMoreButtons";
 import styles from "./styles.module.css"
 import fotoBanner from "../../assets/Banner.png"
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import formatarPreco from "../../hooks/Price";
 
+interface Livro {
+    id: number
+    titulo: string
+    autor: string
+    genero: string
+    preco: number
+    sinopse: string
+    capa: string
+}
 
-export default function Sections() {
+interface SectionsProps {
+    genre: string; 
+  }
+
+export default function Sections({ genre }: SectionsProps){
+    const [livros, setLivros] = useState<Livro[]>([])
+    const generoLivro = genre
+    const [genero, setGenero] = useState<string>(generoLivro);
+
+    
+
+    useEffect(() => {
+        api.get("/livros")
+        .then((response) => {
+            const livrosFiltrados = response.data
+            .filter((livro: Livro) => livro.genero === genero)
+            .slice(0, 4)
+
+            setLivros(livrosFiltrados)
+        })
+        .catch((error) => {
+            console.error('Erro ao buscar livros:', error);
+          })
+    }, [genero])
+
     return (
         <> 
             <section>
                 <div className={styles.indice}>
-                    <h2>Gênero</h2>
+                    <h2>{generoLivro}</h2>
                     <ShowMoreButton/>
                 </div>
                 <div>
                     <ul className={styles.lista}>
-                        <li>
-                            <img src={fotoBanner} className={styles.foto} alt="Capa do Livro"/>
+                    {livros.map((livro, index) => (
+                        <li key={index}>
+                            <img src={livro.capa} className={styles.foto} alt="Capa do Livro"/>
                             <div className={styles.container}>
                                 <div>
-                                    <h2>Nome do Livro</h2>
-                                    <p>Autor do Livro</p>
+                                    <h2>{livro.titulo}</h2>
+                                    <p>{livro.autor}</p>
                                 </div>
-                                <p>Preço do Livro</p>
+                                <p>{formatarPreco(livro.preco)}</p>
                             </div>
                         </li>
+                    ))}
                     </ul>
                 </div>
             </section>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,14 +2,22 @@ import fotoBanner from "../../assets/Banner.png"
 import Sections from "../../components/Sections"
 import styles from "./styles.module.css"
 
+
 export default function Home() {
+
     return (
         <>
             <section className={styles.banner}>
                 <img src={fotoBanner}  alt="Banner de Produtos"/>
                 <p>25% de desconto <br/><span>nos livros do Paulo <br/>Coelho!</span></p>
             </section>
-            <Sections/>
+            <Sections genre="Best-sellers"/>
+            <Sections genre="Clássicos"/>
+            <Sections genre="Fantasia"/>
+            <Sections genre="Infantil"/>
+            <Sections genre="Suspense"/>
+            <Sections genre="Distopia"/>
+            <Sections genre="Ficção Científica"/>
         </>
         
     )

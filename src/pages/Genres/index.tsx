d38f8b297451fab61ccf0483a04aefe571d6e0be
Yasmin Macedo <yasminmacedo_11@outlook.com>
2025-03-05 18:53:00 -0300

    feat(genres): criando design inicial da page

--- src/pages/Genres/index.tsx
+++ src/pages/Genres/index.tsx
@@ -1,7 +1,45 @@
+import styles from "./styles.module.css"
+import fotoBanner from "../../assets/Banner.png"
+import { FaSearch } from "react-icons/fa";
+
+
 export default function Genres() {
     return (
         <>
-        
+        <div className={styles.container}>
+            <span className={styles.searchIcon}><FaSearch /></span>
+            <input 
+                className={styles.search}
+                placeholder={`Pesquisar por título`}
+                //value={filtro}
+                //onChange={(e) => setFiltro(e.target.value)}
+            />
+        </div>
+            <main>
+                <h2> Página do Genêro</h2>
+                <ul className={styles.lista}>
+                    <li>
+                        <img src={fotoBanner} className={styles.foto} alt="Capa do Livro"/>
+                        <div>
+                            <div className={styles.info}>
+                                <h2>Nome do Livro</h2>
+                                <p>Autor do Livro</p>
+                            </div>
+                            <p className={styles.price}>R$32,90</p>
+                        </div>
+                    </li>
+                    <li>
+                        <img src={fotoBanner} className={styles.foto} alt="Capa do Livro"/>
+                        <div>
+                            <div className={styles.info}>
+                                <h2>Nome do Livro</h2>
+                                <p>Autor do Livro</p>
+                            </div>
+                            <p className={styles.price}>R$32,90</p>
+                        </div>
+                    </li>
+                </ul>
+            </main>
         </>
     )
 }
